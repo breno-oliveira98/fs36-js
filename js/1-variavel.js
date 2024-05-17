13d9eeeaef85099dddbd020e81f7f92e48032160
BRENO OLIVEIRA GOMES <BRENO6162@DIGITALCOLLEGE.LOCAL>
2024-05-16 21:54:18 -0300

    Subindo alteraçoes restantes

---
+++ js/1-variavel.js
@@ -0,0 +1,46 @@
+    //para declarar uma variavel usar: var, const ou let
+    //var e let são variaveis que podem ser alteradas
+    //const é uma variavel que não pode ser alterada
+
+    // 1 - criando a variavel e atribuindo o valor
+    // let nome = 'joao';
+
+    /* 
+    ** Declarção de Variaveis **
+    1 - não pode ser palavra reservada
+    2 - não pode começar com número
+    3 - não pode conter espaços ou traços
+    4 - não pode coter caracteres especiais
+    5 - pode começar com letra, underline ou $
+    6 - não pode ser palavra reservada
+    7 - não pode ser nome de função
+    8 - não pode iniciar com operador aritmetico: +,-,/,*
+    9 - não pode iniciar com operador de comparação: >,<,==,!=,>=,<=
+    10 - não pode ser uma variavel ja declarada
+
+    */
+
+    // CamelCase: inicia a 1ª palavra em minúsculo e o restante das palavras inicia em maiúsculo
+let sobreNome2 = 'Ferreira'
+
+    // Alterando o valor
+let nome = 'joao';
+nome = 'alterei o valor'
+
+
+let idade = 26
+
+console.log(typeof idade)
+console.log(nome)
+console.log(sobreNome2)
+
+
+
+
+// 2 - Criando uma constante (algo que nao muda)
+
+const alturaEverest = 8820;
+// Atribuindo o valor da variavel
+alturaEverest = 12500;
+// imprimindo o valor da variavel
+console.log(alturaEverest);
---
+++ js/2-tipos_de_dados.js
@@ -0,0 +1,22 @@
+// undefined é algo que não tem valor
+
+
+let n1 = 10,
+    n2 = 20,
+    n3 = 30;
+
+let resultado = n1 + n2 + n3;    
+
+console.log(resultado)
+
+//soma
+let numero1 = '100';
+let numero2 = 500;
+let resultado1 = numero1 + numero2;
+console.log(resultado1); //faz o calculo
+
+// string com o sinal de '+' o js entende como contatenação, ou seja, vai juntar tudo como texto.
+let numero3 = '100';
+let numero4 = 500;
+let resultado2 = numero3 - numero4;
+console.log(resultado2); //100500
